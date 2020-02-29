8dedbf119abe8072c9c1c116588b4785dbfa3db5
tekilla501 <chinovyou@gmail.com>
2020-02-29 14:48:52 -0600

    segunda insercion

--- src/context/CategoriasContext.js
+++ src/context/CategoriasContext.js
@@ -1,4 +1,5 @@
 import React, {createContext, useState, useEffect} from 'react';
+import Axios from 'axios';
 
 //Crear el context
 export const CategoriasContext = createContext();
@@ -9,12 +10,22 @@ const CategoriasProvider = (props) => {
     //crear el state del context
     const [categorias, guardarCategorias] = useState([]);
 
+    //Ejecutar el llamado a la api
+    useEffect(() =>{
+        const obtenerCategortias = async () => {
+            const url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
+            const categorias = await Axios.get(url);
+            guardarCategorias(categorias.data.drinks);
+        }
+        obtenerCategortias();
+    }, []);
+
     //Ejecutar al llamdo Api
 
     return (
         <CategoriasContext.Provider
             value={{
-
+                categorias
             }}
         >
             {props.children}
---
+++ src/context/ModalContext.js
@@ -0,0 +1,24 @@
+import React, {createContext, useState, useEffect} from 'react';
+import axios from 'axios';
+
+//Crear context 
+export const ModalContext = createContext();
+
+const ModalProvider = (props) => {
+
+    //state del provider
+    const [idreceta, guardarIdReceta] = useState(null);
+
+
+    return (
+        <ModalContext.Provider
+            value={{
+                guardarIdReceta
+            }}
+        >
+            {props.children}
+        </ModalContext.Provider>
+    );
+}
+
+export default ModalProvider;
